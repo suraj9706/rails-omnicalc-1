966a933df48eba83d1d51043149027b8979be9bb
suraj9706 <136658220+suraj9706@users.noreply.github.com>
2023-08-13 16:29:54 +0000

    tester

---
+++ app/controllers/calculator_controller.rb
@@ -0,0 +1,68 @@
+# app/controllers/calculator_controller.rb
+class CalculatorController < ApplicationController
+  def index
+    # This action renders the homepage view (app/views/calculator/index.html.erb)
+  end
+
+  def square_form
+    # This action renders the square form view (app/views/calculator/square_form.html.erb)
+  end
+
+  def square
+    @number = params[:number].to_f
+    @square = @number**2
+    render :square
+  end
+
+
+  def square_form
+    # This action renders the square form view
+  end
+
+  def square
+    @number = params[:number].to_f
+    @square = @number**2
+    render :square
+  end
+
+  def square_root_form
+    # This action renders the square root form view (app/views/calculator/square_root_form.html.erb)
+  end
+
+def square_root
+  @number = params[:number].to_f
+  @result = Math.sqrt(@number)
+  render :square_root
+end
+
+
+  def payment_form
+    # This action renders the payment form view (app/views/calculator/payment_form.html.erb)
+  end
+
+  def payment
+    @apr = params[:apr].to_f
+    @years = params[:years].to_i
+    @present_value = params[:present_value].to_f
+
+    monthly_interest_rate = @apr / 12
+    number_of_payments = @years * 12
+
+    numerator = monthly_interest_rate * @present_value
+    denominator = 1 - (1 + monthly_interest_rate)**(-number_of_payments)
+
+    @monthly_payment = numerator / denominator
+    render :payment
+  end
+
+  def random_form
+    # This action renders the random form view (app/views/calculator/random_form.html.erb)
+  end
+
+  def random
+    @min = params[:min].to_f
+    @max = params[:max].to_f
+    @random_number = rand(@min..@max).round(2)
+    render :random
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  
+  root to: 'calculator#index'
+
+  get '/square/new', to: 'calculator#square_form'
+  post '/square/results', to: 'calculator#square'
+
+  get '/square_root/new', to: 'calculator#square_root_form'
+  post '/square_root/results', to: 'calculator#square_root'
+
+  get '/payment/new', to: 'calculator#payment_form'
+  post '/payment/results', to: 'calculator#payment'
+
+  get '/random/new', to: 'calculator#random_form'
+  post '/random/results', to: 'calculator#random'
 end
